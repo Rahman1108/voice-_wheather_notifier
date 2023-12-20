05dbe846cca3fe0a90664f32515fea2db556560a
Rahman <101407836+Rahman1108@users.noreply.github.com>
2023-12-20 14:14:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,17 @@ def sptext():
             print(data)
         except sr.UnknownValueError:
             print(" Not Understand ")
-sptext()
+def speechtx(x):
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice',voices[1].id)
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate',130)
+    engine.say(x)
+    engine.runAndWait()
+if __name__ == "__main__":
+
+    if "Hey chintu" in sptext().lower() :
+        speechtx("AT your service please command....")
+    else:
+        speechtx("wrong coded your are not my owner....")
